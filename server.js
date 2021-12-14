db4a2de6ef3676a3495f8975ec33afe4bc255d57
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-12-14 00:57:26 -0800

    mysql connection

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Password1',
+    database: 'kemployees_db'
+  },
+  console.log(`Connected to the kemployees_db database.`)
+);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
